548801fcdf04395b6eb1a657963d5ab821b2e0b8
MichaelJordan99 <michaeljordan99@hotmail.com>
2017-05-26 10:26:34 -0700

    padding
    css cleanup
    
    cleanup

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,12 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-  <h1>{{title}}</h1>
-  <nav>
-    <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
-    <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
-  </nav>
-  <router-outlet></router-outlet>
+    <div style="width: 750px;border: 1px solid #e0e2e5;padding: 20px;position: absolute;right: 40px;">
+      Some more content / nav would go here
+    </div>
+    <h1>{{title}}</h1>
+    <nav>
+      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
   `,
   styleUrls: ['app/app.component.css'],
 })
