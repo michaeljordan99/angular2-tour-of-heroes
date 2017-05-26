0da3fb6e333665cf8ef91c7db372759c0ef77f4a
MichaelJordan99 <michaeljordan99@hotmail.com>
2017-05-26 10:25:15 -0700

    Squashed commits

--- app/app.component.js
+++ app/app.component.js
@@ -16,7 +16,7 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n  <h1>{{title}}</h1>\n  <nav>\n    <a routerLink=\"/dashboard\" routerLinkActive=\"active\">Dashboard</a>\n    <a routerLink=\"/heroes\" routerLinkActive=\"active\">Heroes</a>\n  </nav>\n  <router-outlet></router-outlet>\n  ",
+            template: "\n    <div style=\"width: 750px;border: 1px solid #e0e2e5;padding: 20px;position: absolute;right: 40px;\">\n      Some more content / nav would go here\n    </div>\n    <h1>{{title}}</h1>\n    <nav>\n      <a routerLink=\"/dashboard\" routerLinkActive=\"active\">Dashboard</a>\n      <a routerLink=\"/heroes\" routerLinkActive=\"active\">Heroes</a>\n    </nav>\n    <router-outlet></router-outlet>\n  ",
             styleUrls: ['app/app.component.css'],
         }), 
         __metadata('design:paramtypes', [])
